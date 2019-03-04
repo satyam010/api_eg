93546999c7a4388ed60d0399bcce1cb17d65552d
Satyam Prakash <prakashsatyam6@gmail.com>
2019-03-04 20:57:20 +0530

    first commit

---
+++ languages/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Language, Paradigm, Programmer
+
+admin.site.register(Language)
+
+admin.site.register(Paradigm)
+
+admin.site.register(Programmer)
---
+++ languages/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from . import views 
+from rest_framework import routers 
+
+router = routers.DefaultRouter()
+router.register('languages', views.LanguageView)
+router.register('paradigms', views.ParadigmView)
+router.register('programmer', views.ProgrammerView)
+
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
+
+
